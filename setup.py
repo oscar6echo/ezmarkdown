3b45bed8fb1e644a02734325b225838810ee1b2b
oscar6echo <olivier.borderies@gmail.com>
2015-08-08 21:09:49 +0200

    reset version to 0.1

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open('README.rst') as f:
 setup(
   name = 'ezmarkdown',
   packages = ['ezmarkdown'],
-  version = '0.3',
+  version = '0.1',
   description = 'easy markdown: Write markdown in IPython notebook and generate standalone HTML file from it, picking the cells to output',
   long_description = long_description,
   author = 'oscar6echo',
   author_email = 'olivier.borderies@gmail.com',
   url = 'https://github.com/oscar6echo/ezmarkdown', # use the URL to the github repo
-  download_url = 'https://github.com/oscar6echo/ezmarkdown/tarball/0.3', # tag number at the end
+  download_url = 'https://github.com/oscar6echo/ezmarkdown/tarball/0.1', # tag number at the end
   keywords = ['markdown', 'export', 'notebook', 'HTML'], # arbitrary keywords
   license='MIT',
   classifiers = [ 'Development Status :: 4 - Beta',
